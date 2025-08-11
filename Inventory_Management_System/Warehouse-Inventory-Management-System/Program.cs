c1c6aef9c0d5cf6da7450bd89088eb536454dcc9
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-08-11 04:52:37 +0000

    Warehouse Inventory Management System

---
+++ Inventory_Management_System/Warehouse-Inventory-Management-System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Warehouse_Inventory_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
