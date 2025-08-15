27ffe223d0b16bbc501a6c4baf1767b011bafd9e
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-08-15 01:21:16 +0000

    Grade System Project

---
+++ Grading_System/Grading System/Grade_System.cs
@@ -0,0 +1,10 @@
+﻿namespace Grading_System
+{
+    internal class Grade_System
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
